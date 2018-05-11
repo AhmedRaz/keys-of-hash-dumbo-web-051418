1436531efc0df4d0de8d271fdcada0c8c62d9067
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-11 22:16:05 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    animals.collect do |animal, country|
+    if arguments.include?(country)
+      animal
+    end
   end
 end
